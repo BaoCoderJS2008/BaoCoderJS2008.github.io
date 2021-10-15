2f7d7f1a980c6e7392dd5532d2dcf89b06805f2b
BaoCoderJS2008 <90696342+BaoCoderJS2008@users.noreply.github.com>
2021-10-15 20:51:37 +0700

    Add files via upload

--- Main.js
+++ Main.js
@@ -4,10 +4,58 @@ dark.onclick = function() {
   document.body.style.color = "white";
 }
 const light = document.getElementById("light");
-  light.onclick = function() {
+light.onclick = function() {
   document.body.style.backgroundColor = "white";
   document.body.style.color = "black";
-  }
-document.getElementById("VQMM").onclick = function() {
-  window.location.href = "/Games/VQMM.html";
 }
+var request;
+const posts = document.getElementById("posts");
+
+
+if (window.XMLHttpRequest) {
+  request = new XMLHttpRequest();
+} else {
+  request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open("GET", "https://raw.githubusercontent.com/BaoCoderJS2008/BaoCoderJS2008.github.io/main/Posts/Posts.json", false);
+request.onreadystatechange = () => {
+  if (request.status == 200 && request.status !== 4) {
+    const result = JSON.parse(request.responseText);
+    for (let items of result) {
+    posts.innerHTML += `
+    <div class="Admin">
+        <div id="post" style="
+        box-shadow: 2px 2px 2px;
+        ">
+          <div id="avt">
+          <img src="${items.avatar}" style="
+          width: 35px;
+          height: 35px;
+          position: absolute;
+          left: 20px;
+          margin-top: -5px;
+          "/>
+          </div>
+          <div id="author_name" style="
+          margin-left: 50px;
+          ">${items.author} > ${items.subject} / posts / ${items.id}</div>
+          <b><p id="post_title" style="
+          text-align: center;
+          "
+          >${items.title}</p></b>
+          <img src="${items.image}" style="
+          width: 300px;
+          height: 130px;
+          margin-left: 20px;
+          " />
+          <div id="content">
+            ${items.content}
+          </div>
+        </div>
+      </div>
+      <hr />
+    `;
+    }
+  }
+}
+request.send();
