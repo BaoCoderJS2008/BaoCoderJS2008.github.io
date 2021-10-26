f241d199a80e858fa9ac385793f7944ed2b36f4b
BaoCoderJS2008 <90696342+BaoCoderJS2008@users.noreply.github.com>
2021-10-26 16:00:24 +0700

    Add files via upload

---
+++ JS/Lang.js
@@ -0,0 +1,51 @@
+
+ function selectLang() {
+   $("#langList").fadeToggle();
+ }
+
+ const trans = document.getElementById("trans");
+ const PT = document.getElementById("post-title");
+ const PC = document.getElementById("post-content");
+ const title = document.getElementById("title");
+ const register = document.getElementById("register");
+ const addIMG = document.getElementById("addImage");
+ const create = document.getElementById("create");
+ const info = document.getElementById("info");
+ const home = document.getElementById("m1");
+ function en() {
+   $("#en").click(function() {
+     const enURL = "../lang/en.json";
+     fetch(enURL)
+       .then(response => response.json())
+       .then(result => {
+           title.innerHTML = result.title;
+           PC.placeholder = result.post_content;
+           PT.placeholder = result.post_title;
+           trans.innerHTML = result.trans;
+           home.innerHTML = result.home;
+           info.innerHTML = result.info;
+           register.innerHTML = result.register;
+           addIMG.innerHTML = result.addImage;
+           create.innerHTML = result.post;
+       });
+   });
+ }
+
+ function vi() {
+    $("#vi").click(function() {
+     const viURL = "../lang/vi.json";
+     fetch(viURL)
+       .then(response => response.json())
+       .then(result => {
+           title.innerHTML = result.title;
+           PC.placeholder = result.post_content;
+           PT.placeholder = result.post_title;
+           trans.innerHTML = result.trans;
+           home.innerHTML = result.home;
+           info.innerHTML = result.info;
+           register.innerHTML = result.register;
+           addIMG.innerHTML = result.addImage;
+           create.innerHTML = result.post;
+       });
+   });
+ }
