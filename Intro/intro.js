ae28dfa4a51333f9e30285656f52202378814c8d
BaoCoderJS2008 <90696342+BaoCoderJS2008@users.noreply.github.com>
2022-10-19 18:28:36 +0700

    Add files via upload

--- Intro/intro.js
+++ Intro/intro.js
@@ -3,7 +3,7 @@ var tik = setInterval(() => {
   lyf.classList.toggle("digit");
   if (lyf.innerText == 0) {
     document.getElementById('container').classList.toggle("containeroverlay")
-    window.location="./MyBirthday/MyBirthday.html";
+    window.location="../pnvn.html";
     clearInterval(tik)
   } else {
     lyf.innerText -= 1
